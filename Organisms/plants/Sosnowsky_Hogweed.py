a402ae5c9aedb25046ecac0681cbf2dadec40abb
4141 <jasiekfanarty@gmail.com>
2021-05-26 18:27:14 +0200

    Sosnowsky hogweed collision implementation

--- Organisms/plants/Sosnowsky_Hogweed.py
+++ Organisms/plants/Sosnowsky_Hogweed.py
@@ -27,5 +27,24 @@ class Sosnowsky_Hogweed(Plant):
             if self.mapping.cells[self.pos.y+1][self.pos.x].org != "null" and not issubclass(type(self.mapping.cells[self.pos.y+1][self.pos.x].org), Plant):
                 positions.append(Position(self.pos.x, self.pos.y+1))
         if len(positions) != 0:
+            print("pozycje przed filtrem: ", positions, len(positions))
+            positions = [pos for pos in positions if self.mapping[pos.y][pos.x].org != "null" and self.mapping[pos.y][pos.x].org.name == "Cyber_sheep"]
+            print("pozycje po filtrze: ", positions, len(positions))
             return [positions, [f"{self.name} killed some animals around it!"]]
         return []
+
+    def collision(self, attacker, old_pos, mapping):
+        result = []
+        self.alive = False
+        if attacker.name != "Cyber_sheep":
+            result.append(self)
+            result.append(attacker)
+        else:
+            result.append(attacker)
+            result.append(self)
+        msg = "Sosnowsky's hogweed has been eaten: "
+        result.append([
+            f"{msg}{attacker.pos} {attacker.id}: {attacker.name} => {self.pos} {self.id}: {self.name}",
+            f"{len(msg) * ' '}{attacker.strength}{(len(attacker.name) - len(str(attacker.strength))) * ' '} => {self.strength}"
+        ])
+        return result
